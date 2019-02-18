09c1cd429d8c6858319d7bbf7843c4f9c1c9a12b
dewan <dewan@cs.unc.edu>
2019-02-18 10:24:49 -0500

    A2Suite updated

--- src/gradingTools/comp533s19/assignment2/Assignment2Suite.java
+++ src/gradingTools/comp533s19/assignment2/Assignment2Suite.java
@@ -3,14 +3,26 @@ package gradingTools.comp533s19.assignment2;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import grader.basics.junit.BasicJUnitUtils;
 import gradingTools.comp533s19.assignment1.Assignment1OneClientSuite;
+import gradingTools.comp533s19.assignment1.Assignment1Suite;
 import gradingTools.comp533s19.assignment1.testcases.OneClientConnection;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	Assignment1OneClientSuite.class
+	Assignment2OneClientSuite.class,
+	Assignment2OneClientSuite.class
 })
-public class Assignment2Suite {
+public class Assignment2Suite extends Assignment1Suite{
+	public static void main (String[] args) {
+		try {
+			
+			BasicJUnitUtils.interactiveTest(Assignment2Suite.class);
 
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
