75d5f555d25aa11f596f275ff39ea55200e2aecf
0c1e468e4e4b1d39812fd2c8c5a0cba81828a5682eced2a6ca1e50afe43e42, 13ae3ed6fe76d459c9c66fe38ff187593561a1f24d34cb22e06148c77e4cc02b(2627dc3fbf149f58a61f9f94d28baed5bb59cf68f64b4ff611d3bc54a0cb81da)/ <ajwortas@gmail.com>
2022-04-13 18:03:42 -0400

    A7S22

---
+++ src/gradingTools/comp533s22/assignment7/S22Assignment7Suite.java
@@ -0,0 +1,46 @@
+package gradingTools.comp533s22.assignment7;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import grader.basics.config.BasicExecutionSpecificationSelector;
+import grader.basics.junit.BasicJUnitUtils;
+import gradingTools.comp533s19.assignment1.testcases.OneClientConnection;
+import gradingTools.comp533s19.assignment2.Assignment2OneClientSuite;
+import gradingTools.comp533s19.assignment2.Assignment2Suite;
+import gradingTools.comp533s19.assignment2.Assignment2TwoClientSuite;
+import gradingTools.comp533s20.assignment7.Assignment7OneClientSuite;
+import gradingTools.comp533s20.assignment7.Assignment7TwoClientSuite;
+import gradingTools.comp533s21.assignment7.S21A7AssignmentTags;
+import gradingTools.comp533s21.assignment7.style.A7GeneralStyleSuite;
+import gradingTools.comp533s22.assignment7.style.A7SpecificStyleSuite;
+import gradingTools.comp533s21.codeReuseHelper.TagsFactory;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	Assignment7OneClientSuite.class,
+	Assignment7TwoClientSuite.class,
+	A7GeneralStyleSuite.class,
+	A7SpecificStyleSuite.class
+})
+public class S22Assignment7Suite extends Assignment2Suite{
+	
+	public static void configureProperties() {
+		BasicExecutionSpecificationSelector.getBasicExecutionSpecification().
+		setCheckStyleConfiguration("unc_checks_533_A7.xml");
+	}
+	public static void main (String[] args) {
+		try {
+			
+			BasicJUnitUtils.interactiveTest(S22Assignment7Suite.class);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	static {
+		configureProperties();
+		TagsFactory.setAssignmentTags(new S21A7AssignmentTags());
+	}
+}
---
+++ src/gradingTools/comp533s22/assignment7/style/A7SpecificStyleSuite.java
@@ -0,0 +1,48 @@
+package gradingTools.comp533s22.assignment7.style;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import grader.basics.junit.BasicJUnitUtils;
+import gradingTools.comp533s21.assignment7.style.A7ExpectedCalls;
+import gradingTools.comp533s21.assignment7.style.A7ExpectedSignatures;
+import gradingTools.comp533s21.assignment7.style.A7TaggedClassesDefined;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	A7TaggedClassesDefined.class,
+//	A7ExpectedInterfaces.class,
+	A7ExpectedCalls.class,
+//	A7SpuriousCalls.class,
+//	A6ExpectedGetters.class,
+//	A6ExpectedSetters.class,
+	A7ExpectedSignatures.class,
+	A7ProjectOrganization.class,
+	
+	
+})
+	
+
+//@MaxValue(50)
+public class A7SpecificStyleSuite {
+
+	public static void main (String[] args) {
+		try {
+//			BasicExecutionSpecificationSelector.getBasicExecutionSpecification().setLanguage(BasicLanguageDependencyManager.JAVA_LANGUAGE);
+
+			BasicJUnitUtils.interactiveTest(A7SpecificStyleSuite.class);
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+//	static {
+//		BasicExecutionSpecificationSelector.getBasicExecutionSpecification().
+//		setCheckStyleConfiguration("unc_checks_524_A1.xml");
+//	}
+}
+
+	
+
